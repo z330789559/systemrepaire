a5cdd39fdad5cff1977daf84c24a1170b25d4432
baozhong <baozhong.li@life.me>
2017-04-15 15:05:08 +0800

    优化

--- config/default.js
+++ config/default.js
@@ -4,9 +4,9 @@
 var path=require("path")
 
 module.exports={
-    port:process.env.PORT||4444,
+    port:process.env.PORT||3000,
     mongodb:{
-        url:'mongodb://127.0.0.1/club'
+        url:'mongodb://127.0.0.1:27017/club'
     },
     schemeConf:path.join(__dirname,'./default.scheme'),
     staticCacheConf:path.join(__dirname,'../theme/publices'),
